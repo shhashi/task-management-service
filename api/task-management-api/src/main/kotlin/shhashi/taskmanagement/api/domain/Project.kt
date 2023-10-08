b7ed68b288b79516c275c218cbbacc244ff04fbb
shhashi <55823354+shhashi@users.noreply.github.com>
2023-10-09 00:07:15 +0900

    プロジェクトドメインモデルに参画メンバの項目を追加

--- api/task-management-api/src/main/kotlin/shhashi/taskmanagement/api/domain/Project.kt
+++ api/task-management-api/src/main/kotlin/shhashi/taskmanagement/api/domain/Project.kt
@@ -1,5 +1,6 @@
 package shhashi.taskmanagement.api.domain
 
+import shhashi.taskmanagement.api.domain.value.AccountId
 import shhashi.taskmanagement.api.domain.value.ProjectId
 import shhashi.taskmanagement.api.domain.value.ProjectName
 
@@ -8,12 +9,14 @@ import shhashi.taskmanagement.api.domain.value.ProjectName
  *
  * @property projectId プロジェクト ID
  * @property projectName プロジェクト名
+ * @property assignedAccountIds プロジェクトに参画しているメンバーのアカウント ID のリスト。
  * @property stages ステージ一覧
  * @property isClosed クローズ済みかどうか
  */
 data class Project(
     var projectId: ProjectId,
     var projectName: ProjectName?,
+    var assignedAccountIds: List<AccountId>?,
     var stages: List<Stage>?,
     var isClosed: Boolean?
 )
